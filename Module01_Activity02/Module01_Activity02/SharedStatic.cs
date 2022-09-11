33066ebe22da71de6ef041ad489e45bab3889592
Mikaella Mae Silva <mikaellamaecsilva@gmail.com>
2022-09-11 14:43:45 +0800

    first commit

---
+++ Module01_Activity02/Module01_Activity02/SharedStatic.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Module01_Activity02
+{
+    public static class SharedStatic
+    {
+        public static readonly Color ButtonColor = Color.Black;
+        public static readonly Color ButtonTextColor = Color.White;
+    }
+}
